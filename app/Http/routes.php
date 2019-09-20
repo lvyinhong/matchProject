db56294769f6fe1abd72699426141a8f3ce2624a
lyh <lvxiaohong@hupu.com>
2019-09-20 13:20:50 +0800

    测试

---
+++ app/Http/routes.php
@@ -0,0 +1,26 @@
+<?php
+
+get('/', 'StaticPagesController@home')->name('home');
+get('/help', 'StaticPagesController@help')->name('help');
+get('/about', 'StaticPagesController@about')->name('about');
+
+get('signup', 'UsersController@create')->name('signup');
+resource('users', 'UsersController');
+
+get('login', 'SessionsController@create')->name('login');
+post('login', 'SessionsController@store')->name('login');
+delete('logout', 'SessionsController@destroy')->name('logout');
+
+get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+get('password/email', 'Auth\PasswordController@getEmail')->name('password.reset');
+post('password/email', 'Auth\PasswordController@postEmail')->name('password.reset');
+get('password/reset/{token}', 'Auth\PasswordController@getReset')->name('password.edit');
+post('password/reset', 'Auth\PasswordController@postReset')->name('password.update');
+
+resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+get('/users/{id}/followings', 'UsersController@followings')->name('users.followings');
+get('/users/{id}/followers', 'UsersController@followers')->name('users.followers');
+post('/users/followers/{id}', 'FollowersController@store')->name('followers.store');
+delete('/users/followers/{id}', 'FollowersController@destroy')->name('followers.destroy');
